d0e88018c27b965d295d368caed55847f2a1e02a
ibnuafdel <ibnuafdel@gmail.com>
2025-08-10 16:22:12 +0300

    chore(cleanup): remove legacy bin/tests/IDE files and phpunit.xml to slim the starter

--- Http/controllers/demo.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$id = $_GET['id'] ?? null;
-
-view('demo.view.php', ['id' => $id]); 
--- resources/views/demo.view.php
+++
@@ -1,9 +0,0 @@
-<?php require base_path('resources/views/partials/head.php') ?>
-<?php require base_path('resources/views/partials/nav.php') ?>
-
-<main class="mx-auto max-w-3xl p-6">
-  <h1 class="text-2xl font-bold">Demo Route</h1>
-  <p class="mt-4">Captured id: <span class="font-mono"><?= htmlspecialchars($id ?? '') ?></span></p>
-</main>
-
-<?php require base_path('resources/views/partials/footer.php') ?> 
